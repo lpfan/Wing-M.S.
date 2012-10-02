0266e3a2912eff7237334f4ae965c9041b1810d5
Misha Shchetinin <a@as-MacBook-Pro.local>
2012-10-02 12:58:28 +0300

    tinyMCE integrated successfully;

--- project/__init__.py
+++ project/__init__.py
@@ -11,7 +11,7 @@ from flask.ext.admin.contrib import fileadmin
 from admin.index import GeneralView
 
 def rel(*x):
-	return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), *x)
 
 app = Flask(__name__)
 UPLOAD_FOLDER = os.path.join(rel('static'), 'uploads')
@@ -26,31 +26,31 @@ from admin.file_upload import *
 from frontend.controllers import index
 
 admin.add_view(My_ModelView(
-	Article,
-	name="Articles"
-	)
+    Article,
+    name="Articles"
+    )
 )
 admin.add_view(My_ModelView(
-	Category,
-	name="Categories",
-	endpoint='category_mgm')
+    Category,
+    name="Categories",
+    endpoint='category_mgm')
 )
 admin.add_view(UsersView(
-	User,
-	name="Users")
+    User,
+    name="Users")
 )
 
 admin.add_view(SettingsView(
-	name="Settings")
+    name="Settings")
 )
 
 admin.add_view(GalleryView(
-	name = 'Gallery'
-	)
+    name = 'Gallery'
+    )
 )
 
 admin.add_view(fileadmin.FileAdmin(
-	UPLOAD_FOLDER, '/uploads/', name='File Manager')
+    UPLOAD_FOLDER, '/uploads/', name='File Manager')
 )
 
 app.debug = True
